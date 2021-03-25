adbb02b646a40519a8324bc17b70b95039935ed6
amamov <ysangsuk78@gmail.com>
2021-03-26 02:32:05 +0900

    project set up

---
+++ config/urls.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+]
+
+
+if settings.DEBUG:
+    from django.conf import settings
+    from django.conf.urls.static import static
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
